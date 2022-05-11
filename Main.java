d6da40edd4219dd51b0994a5daf674dec77438d0
Ivanov Vlad <lrn@2ch.hk>
2022-05-11 12:12:10 +0300

    initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+package structural.facade;
+
+public class Main {
+    public static void main(String[] args) {
+        BinOps binOps = new BinOps();
+        System.out.println(binOps.sum("1", "10"));
+        System.out.println(binOps.mult("10", "10"));
+
+    }
+}
